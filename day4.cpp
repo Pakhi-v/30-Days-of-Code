4eff2b440108a4731b79a96d3fd5478eb051b7d8
PAKHI VASHISHTH <72306516+Pakhi-v@users.noreply.github.com>
2022-03-31 23:37:21 +0530

    Add files via upload

--- day4.cpp
+++ day4.cpp
@@ -1,26 +1,58 @@
-#include <bits/stdc++.h>
-
 using namespace std;
-int main(){
+#include <iostream>
 
-int N;
-cin>>N;
- if(N%2==0)
-    {
-        if(N>=2 && N<=5)
-        {
-            cout<<"Not Weird";
-        }
-        else if(N>=6 && N<=20)
-        {
-            cout<<"Weird";
-        }    
-        else
-        {
-            cout<<"Not Weird";
-        }
+class Person{
+    public:
+        int age;
+        Person(int initialAge);
+        void amIOld();
+        void yearPasses();
+    };
+
+    Person::Person(int initialAge){
+         if(initialAge==NULL || initialAge<0){
+      age=0;
+      cout<<"Age is not valid, setting age to 0.\n";
+  }else {
+      age=initialAge;
+  }
+
     }
-    else
-        cout<<"Weird";
+
+    void Person::amIOld(){
+         if(age<13){
+        cout<<"You are young.\n";
+    } else if(age>=13 && age<18){
+        cout<<"You are a teenager.\n";
+    } else{
+        cout<<"You are old.\n";
+    }
+
+}
+     
+
+    
+
+    void Person::yearPasses(){
+          age+=1;
+
+    }
+
+int main(){
+    int t;
+	int age;
+    cin >> t;
+    for(int i=0; i < t; i++) {
+    	cin >> age;
+        Person p(age);
+        p.amIOld();
+        for(int j=0; j < 3; j++) {
+        	p.yearPasses(); 
+        }
+        p.amIOld();
+      
+		cout << '\n';
+    }
+
     return 0;
 }
