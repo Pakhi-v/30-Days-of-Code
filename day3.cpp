c5d2b270dec88de13bc0e5ed8557f7ce5537592b
PAKHI VASHISHTH <72306516+Pakhi-v@users.noreply.github.com>
2022-03-31 23:38:17 +0530

    Add files via upload

--- day3.cpp
+++ day3.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void ans(double meal_cost, int tip_percent, int tax_percent) 
-{
-    double tip=meal_cost*tip_percent/100;
-    double tax=meal_cost*tax_percent/100;
-    int totalCost=(int)round(meal_cost+tip+tax);    
-    cout<<totalCost;
-}
-int main()
-{
-    double meal_cost;
-    cin >> meal_cost;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    int tip_percent;
-    cin >> tip_percent;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    int tax_percent;
-    cin >> tax_percent;
-    
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    ans(meal_cost, tip_percent, tax_percent);
-    
+int main(){
+
+int N;
+cin>>N;
+ if(N%2==0)
+    {
+        if(N>=2 && N<=5)
+        {
+            cout<<"Not Weird";
+        }
+        else if(N>=6 && N<=20)
+        {
+            cout<<"Weird";
+        }    
+        else
+        {
+            cout<<"Not Weird";
+        }
+    }
+    else
+        cout<<"Weird";
     return 0;
 }
